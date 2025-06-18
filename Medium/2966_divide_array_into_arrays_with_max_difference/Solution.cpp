6c9b2e9dd81e7ca3973bb5f1a61962e9653723c1
Kaushikmak <96010041+Kaushikmak@users.noreply.github.com>
2025-06-18 21:38:25 +0530

    day 19

---
+++ Medium/2966_divide_array_into_arrays_with_max_difference/Solution.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > divideArray(vector<int> &nums, int k) {
+        vector<vector<int>> result;
+        // sort the array
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i += 3) {
+            if (nums[i+2]-nums[i]>k) {
+                return {};
+            }
+            result.push_back({nums[i],nums[i+1],nums[i+2]});
+        }
+        return result;
+
+    }
+};
